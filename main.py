439702ef5ef737a35586bb24bf57ddf91209fd4d
JimAchterbergLUMC <j.l.achterberg@lumc.nl>
2025-07-09 12:36:43 +0200

    update structure

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from evaluation import evaluate
 from sklearn.datasets import load_diabetes
 import pandas as pd
 
-
 # start with clean workspace
 clear_dir("workspace")
 
@@ -71,16 +70,13 @@ hparams_all = {
     },
 }
 
-# for generator in ["arf", "bayesian_network", "ctgan", "tvae", "ddpm"]:
-
-generator = "ddpm"
+generator = "arf"
 hparams = hparams_all[generator]
 cv_folds = 3
 n_init = 3
 seed = 0
 enable_reproducible_results(seed)
 results = {}
-# we can add the same metric multiple times with different parameters by adding a dash to the metric name
 metrics = {
     "wasserstein": {},
     "prdc": {},
@@ -100,48 +96,52 @@ metrics = {
 # START BENCHMARKING
 
 # load data
-# dataset = openml.datasets.get_dataset(4541)
-# X, _, _, _ = dataset.get_data(dataset_format="dataframe")
-# X = X.drop(["encounter_id", "patient_nbr"], axis=1)
-# X = X[:100]
-
 X, y = load_diabetes(as_frame=True, return_X_y=True, scaled=False)
 X = pd.concat([X, y], axis=1)
 X["sex"] = X["sex"].map({1: "female", 2: "male"})
 discrete_features = ["sex"]
 
-# perform k fold CV
-for fold, (train, test) in enumerate(
-    KFold(n_splits=cv_folds, shuffle=True, random_state=seed).split(X)
-):
-    print(f"fold: {fold}")
-    results[f"fold: {fold}"] = {}
-    # get train-test data
-    X_train = GenericDataLoader(data=X.iloc[train])
-    X_test = GenericDataLoader(data=X.iloc[test])
-
-    # synthesize for multiple initializations
-    for i in range(n_init):
-        print(f"init: {i}")
-        hparams["random_state"] = i
-        # clear GPU cache before SD generation
-        clear_cache()
-        plugin = Plugins().get(generator, **hparams)
-        # unconditional generation (we do not consider a specific target feature)
-        plugin.fit(X_train)
-        X_syn = plugin.generate(len(X))
-        # clear GPU cache after SD generation (some eval metrics also require GPU memory)
-        clear_cache()
-        # clear workspace cache
-        clear_dir("workspace")
-        # evaluation
-        results[f"fold: {fold}"][f"init: {i}"] = evaluate(
-            X_train.dataframe(),
-            X_test.dataframe(),
-            X_syn.dataframe(),
-            metrics,
-            discrete_features=discrete_features,
-        )
+if cv_folds == 1 and n_init == 1:
+    # No CV, single run
+    print("Single run: no cross-validation.")
+    X = GenericDataLoader(data=X)
+    X_train, X_test = X.train(), X.test()
+    hparams["random_state"] = seed
+    plugin = Plugins().get(generator, **hparams)
+    plugin.fit(X_train)
+    X_syn = plugin.generate(len(X))
+    results["report"] = evaluate(
+        X_train.dataframe(),
+        X_test.dataframe(),
+        X_syn.dataframe(),
+        metrics,
+        discrete_features=discrete_features,
+    )
+else:
+    # perform k fold CV
+    for fold, (train, test) in enumerate(
+        KFold(n_splits=cv_folds, shuffle=True, random_state=seed).split(X)
+    ):
+        print(f"fold: {fold}")
+        results[f"fold: {fold}"] = {}
+        X_train = GenericDataLoader(data=X.iloc[train])
+        X_test = GenericDataLoader(data=X.iloc[test])
+        for i in range(n_init):
+            print(f"init: {i}")
+            hparams["random_state"] = i
+            clear_cache()
+            plugin = Plugins().get(generator, **hparams)
+            plugin.fit(X_train)
+            X_syn = plugin.generate(len(X))
+            clear_cache()
+            clear_dir("workspace")
+            results[f"fold: {fold}"][f"init: {i}"] = evaluate(
+                X_train.dataframe(),
+                X_test.dataframe(),
+                X_syn.dataframe(),
+                metrics,
+                discrete_features=discrete_features,
+            )
 
 # save results
 os.makedirs("results", exist_ok=True)
---
+++ viz_scripts/format_results.py
@@ -0,0 +1,35 @@
+# formats results from JSON files to readable tables with averages, std, etc.
+
+import json
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+import os
+import re
+
+# open json file
+generator = "arf"
+with open(f"results/{generator}.json", "r") as f:
+    data = json.load(f)
+
+
+def print_table(data):
+
+    # create df
+    fold_data = []
+    for fold, metrics in data.items():
+        for init, values in metrics.items():
+            # Extract fold and init values
+            fold_value = int(fold.split(":")[1].strip())
+            init_value = int(init.split(":")[1].strip())
+            values["fold"] = fold_value
+            values["init"] = init_value
+            fold_data.append(values)
+    df = pd.DataFrame(fold_data)
+
+    print(df.mean().round(3))
+
+    print(df.std().round(3))
+
+
+print_table(data)
